df62ba7f1d919237ed8829beeae009306b3a37c5
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-03 11:19:59 -0600

    initial commit step 1a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,13 +6,22 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i=0; i<4; i++) {
   console.log(i);
 }
 
-// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
+//   -- the parameter that ends the loop, which is that i is less than 4. To count up to 5, it should be less than 6.
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {
+  console.log(i);
+} /* Just talking through the logic to make sure I understand
+ The idea is that we are logging our variable 'i' to the console
+ our loop defines i in a current state '0'
+ then defines an ending parameter 'less than 5'
+ and a condition to continue the loop until that parameter is met 'increment up'
+ resulting in 6 total instances, 0 through 5 */
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
@@ -26,7 +35,7 @@ console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 console.log('STRETCH: countdown from 5 to 0');
 
 
-// 2. For of loops  
+// 2. For of loops
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
@@ -34,7 +43,7 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 console.log('Some stars:');
 
 
-// 3. While loops  
+// 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
